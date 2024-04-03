abb31c8da4327f69e3f95fd0bb49aac75990b5ef
burningmannn <daniil24022002@gmail.com>
2024-04-03 13:28:56 +0500

    Add Amazing Animations

--- TodoList/Views/ListView.swift
+++ TodoList/Views/ListView.swift
@@ -12,17 +12,24 @@ struct ListView: View {
     @EnvironmentObject var listViewModel: ListViewModel
     
     var body: some View {
-        List {
-            ForEach(listViewModel.items) { item in
-                    ListRowView(item: item)
-                    .onTapGesture {
-                        withAnimation(.linear) {
-                            listViewModel.updateItem(item: item)
-                        }
+        ZStack {
+            if listViewModel.items.isEmpty {
+                NoItemsView()
+                    .transition(AnyTransition.opacity.animation(.easeIn))
+            } else {
+                List {
+                    ForEach(listViewModel.items) { item in
+                            ListRowView(item: item)
+                            .onTapGesture {
+                                withAnimation(.linear) {
+                                    listViewModel.updateItem(item: item)
+                                }
+                            }
                     }
+                    .onDelete(perform: listViewModel.deleteItem)
+                    .onMove(perform: listViewModel.moveItem)
+                }
             }
-            .onDelete(perform: listViewModel.deleteItem)
-            .onMove(perform: listViewModel.moveItem)
         }
         .listStyle(PlainListStyle())
         .navigationTitle("Todo List 📝")
---
+++ TodoList/Views/NoItemsView.swift
@@ -0,0 +1,67 @@
+//
+//  NoItemsView.swift
+//  TodoList
+//
+//  Created by Daniil Shutkin on 03.04.2024.
+//
+
+import SwiftUI
+
+struct NoItemsView: View {
+    
+    @State var animate: Bool = false
+    let secondaryAccentColor = Color("SecondaryAccentColor")
+    
+    var body: some View {
+        ScrollView {
+            VStack(spacing: 10) {
+                Text("There are no items!")
+                    .font(.title)
+                    .fontWeight(.semibold)
+                Text("Are you a productive person? I think you should clock the add button and add a bunch of items to your todo list")
+                    .padding(.bottom, 20)
+                NavigationLink(destination: AddView(), label: {
+                    Text("Add task 🎉")
+                        .foregroundColor(.white)
+                        .font(.headline)
+                        .frame(height: 55)
+                        .frame(maxWidth: .infinity)
+                        .background(animate ? secondaryAccentColor : Color.accentColor)
+                        .cornerRadius(10)
+                })
+                .padding(.horizontal, animate ? 30 : 50)
+                .shadow(
+                    color: animate ? secondaryAccentColor.opacity(0.7) : Color.accentColor.opacity(0.7),
+                    radius: animate ? 30 : 10,
+                    x: 0.0,
+                    y: animate ? 50 : 10)
+                .scaleEffect(animate ? 1.1 : 1.0)
+                .offset(y: animate ? -7 : 0)
+            }
+            .multilineTextAlignment(.center)
+            .padding(40)
+            .onAppear(perform: addAnimation)
+        }
+        .frame(maxWidth: .infinity, maxHeight: .infinity, alignment: .center)
+    }
+    
+    func addAnimation() {
+        guard !animate else { return }
+        DispatchQueue.main.asyncAfter(deadline: .now() + 1.5) {
+            withAnimation(
+                Animation
+                    .easeInOut(duration: 2.0)
+                    .repeatForever()
+            ) {
+                animate.toggle()
+            }
+        }
+    }
+}
+
+#Preview {
+    NavigationView {
+        NoItemsView()
+            .navigationTitle("Title")
+    }
+}
